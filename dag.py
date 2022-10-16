21b5872f3a009a13dfb1bd73a1323b79223c45cc
pppavlov <pppavlov@github.com>
2022-10-16 16:05:24 +0300

    add dash v2

--- dag.py
+++ dag.py
@@ -1,33 +1,30 @@
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 import datetime as dt
 default_args = {
     'owner': 'airflow',
     'start_date': dt.datetime(2022,10,16),
-    'retries': 30
+    'retries': 30,
     'retry_delay': dt.timedelta(minutes=3),
 }
-
-dag = DAG('DashPlot',
+with DAG('DashPlot',
           default_args=default_args,
-          schedule_interval='0 * * * *', # minutes '0 1 * * *' hours
+          schedule='0 * * * *', # minutes '0 1 * * *' hours
           catchup=False
-          )
+          ) as dag:
 
-command_t1 = 'python /Users/admin/pppavlov.github.io/BokehDash.py'
-t1 = BashOperator(
-          task_id = 'BokehDash',
-          bash_command = command_t1
-          dag = dag
-)
+    command_t1 = 'python /Users/admin/pppavlov.github.io/BokehDash.py'
+    t1 = BashOperator(
+              task_id = 'BokehDash',
+              bash_command = command_t1
+    )
 
-command_t2 = 'python /Users/admin/pppavlov.github.io/git_push.py '
-t2 = BashOperator(
-                  task_id = 'gitPush',
-                  bash_command = command_t2,
-                  dag = dag
-)
+    command_t2 = 'python /Users/admin/pppavlov.github.io/git_push.py '
+    t2 = BashOperator(
+                      task_id = 'gitPush',
+                      bash_command = command_t2,
+    )
 
 
 t1 >> t2 
